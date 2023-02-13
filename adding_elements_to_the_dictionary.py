347ae5f1ca639f60b857adeb22b8b17e0511dd77
anurag <anuraganand638@gmail.com>
2023-02-13 20:03:59 +0530

    Oprations on dictionary

---
+++ adding_elements_to_the_dictionary.py
@@ -0,0 +1,12 @@
+# Add(append) elements to a dictionary
+
+mybio = {
+    "name": "anurag anand",
+    "VILLAGE": "ghosaith",
+    "hobbies": "nothing",
+    "likes": "food"
+}
+mybio.update({"weight": "seventy six kilograms",
+              "class": "bachlor in arts",
+              "section": "e"})
+print(mybio)
