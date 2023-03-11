a62e099329574c6f543cd4901063854eb9d2c53e
anurag <anuraganand638@gmail.com>
2023-03-11 17:12:47 +0530

    code to give some output

---
+++ new_codes.py
@@ -0,0 +1,15 @@
+import random
+
+# Generate a random number between 1 and 100
+number = random.randint(1, 100)
+
+# Print the number
+print("The random number is:", number)
+
+# write a code to add numbers 1 to 100
+
+sum  = 0
+for i in range(1, 101):
+    sum += i
+print("The sum of all numbers from 1 to 100 is:", sum)
+
--- some_basic_codes.py
+++ some_basic_codes.py
@@ -1,16 +1,23 @@
-# 13. write a code where ask user to input their daily wage and then how much they will earn in a month
+# # 13. write a code where ask user to input their daily wage and then how much they will earn in a month
+#
+# dailywage = input("total earnings in a day")
+# month = input("days worked in a month")
+# totalsalary = int(dailywage) * int(month)
+# print("total earnings in a month", totalsalary,"rupees monthly")
+#
+#
+#
+# #14.Create a program that prompt user to enter name and age,then prints  a greeting message and their age in 10 years.
+#
+#
+# yourname = input("what is your name")
+# print("a very good morning to you", yourname, "sir")
+# yourage = input("how old are you")
+# print("you will be", int(yourage) + 10, " years old in the upcoming ten years ")
 
-dailywage = input("total earnings in a day")
-month = input("days worked in a month")
-totalsalary = int(dailywage) * int(month)
-print("total earnings in a month", totalsalary,"rupees monthly")
-
-
-
-#14.Create a program that prompt user to enter name and age,then prints  a greeting message and their age in 10 years.
-
-
-yourname = input("what is your name")
-print("a very good morning to you", yourname, "sir")
-yourage = input("how old are you")
-print("you will be", int(yourage) + 10, " years old in the upcoming ten years")
+# write a code to add from 1 to 100
+# This program calculates the sum of all numbers from 1 to 100
+sum = 0
+for i in range(1, 101):
+    sum += i
+print("The sum of all numbers from 1 to 100 is:", sum)
