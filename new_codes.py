58a315aa87689a6f3667e165c5be52975a730658
anurag <anuraganand638@gmail.com>
2023-03-15 18:09:08 +0530

    code to give some output

--- new_codes.py
+++ new_codes.py
@@ -1,37 +1,42 @@
-import random
+# import random
+#
+# # Generate a random number between 1 and 100
+# number = random.randint(1, 100)
+#
+# # Print the number
+# print("The random number is:", number)
+#
+# # write a code to add numbers 1 to 100
+#
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+# print("The sum of all numbers from 1 to 100 is:", sum)
+# # making a CHATBOT like chatGPT
+# print("Hello! I am elon musk . How can I help you today?")
+# while True:
+#     user_input = input("You: ")
+#     if user_input.lower() == "hi" or user_input.lower() == "hello":
+#         print("musk: Hello there!")
+#     elif user_input.lower() == "how are you?" or user_input.lower() == "how are you doing?":
+#         print("musk: I'm doing well, thank you for asking!")
+#     elif user_input.lower() == "what's your name?" or user_input.lower() == "who are you?":
+#         print("myself: My name is Elon musk, and I'm here to help you!")
+#     elif user_input.lower() == "bye" or user_input.lower() == "goodbye":
+#         print("myself: Goodbye! Have a nice day!")
+#         break
+#     else:
+#         print("myself: Sorry, I didn't get that.")
+# # make a function which takes words and tell their length
+# words = input("Enter words separated by spaces: ")
+# word_list = words.split()
+# word_lengths = [len(word) for word in word_list]
+#
+# print("Lengths of the words:")
+# for i, length in enumerate(word_lengths):
+#     print(f"Word {i + 1}: {length}")
 
-# Generate a random number between 1 and 100
-number = random.randint(1, 100)
 
-# Print the number
-print("The random number is:", number)
-
-# write a code to add numbers 1 to 100
-
-sum = 0
-for i in range(1, 101):
-    sum += i
-print("The sum of all numbers from 1 to 100 is:", sum)
-# making a CHATBOT like chatGPT
-print("Hello! I am elon musk . How can I help you today?")
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "hi" or user_input.lower() == "hello":
-        print("musk: Hello there!")
-    elif user_input.lower() == "how are you?" or user_input.lower() == "how are you doing?":
-        print("musk: I'm doing well, thank you for asking!")
-    elif user_input.lower() == "what's your name?" or user_input.lower() == "who are you?":
-        print("myself: My name is Elon musk, and I'm here to help you!")
-    elif user_input.lower() == "bye" or user_input.lower() == "goodbye":
-        print("myself: Goodbye! Have a nice day!")
-        break
-    else:
-        print("myself: Sorry, I didn't get that.")
-# make a function which takes words and tell their length
-words = input("Enter words separated by spaces: ")
-word_list = words.split()
-word_lengths = [len(word) for word in word_list]
-
-print("Lengths of the words:")
-for i, length in enumerate(word_lengths):
-    print(f"Word {i + 1}: {length}")
+numbers = [2, 3, 54, 76, 342, 675, 0, -43, -534342,  234, 980, ]
+numbers.sort()
+print(numbers)
